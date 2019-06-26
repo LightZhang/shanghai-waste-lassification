e7d7abb17dcf0e91eae858679ea539cf6c6fc404
方枫林 <fenglin.fang@kalendsoft.com>
2019-06-26 15:31:55 +0800

    pass test

--- cloudfunctions/search/index.js
+++ cloudfunctions/search/index.js
@@ -6,19 +6,22 @@ const MAX_LIMIT = 10
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let _name = event.name
-  const data = []
-  const item = db.collection('_waste').where({
-    name:db.RegExp({
-      regexp:_name
-    })
-  }).limit(MAX_LIMIT).get()
-  data.push(item)
+    try{
+      //返回经过热度排序的数据库记录
+      let myDb = db.collection('_waste')
+      let data = await myDb.where(
+        {
+          name: db.RegExp({ regexp: event.name })
+        }
+      ).orderBy('hot', 'desc')
+      .limit(MAX_LIMIT).get()
 
-  return (await Promise.all(data)).reduce((acc,cur) => {
-    return {
-      data:acc.data.concat(cur.data),
-      errMsg:acc.errMsg
+      //更新搜索过得数据热度
+      //TODO
+      return data
     }
-  })
+  catch(e){
+    console.error(e)
+    return "error"
+  }
 }
